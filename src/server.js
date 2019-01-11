ea1727d880421e74729527a236e1c6faadccfbd0
malavikavarma <43058115+malavikavarma@users.noreply.github.com>
2019-01-11 14:18:44 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -2,9 +2,9 @@ import executelangDetect from './detect_lang';
 import searchAPI from './search_api';
 
 const express = require('express');
-// var requestChecker  = require('./middleware');
+var requestChecker  = require('./middleware');
 const app = express();
-// app.use(requestChecker);
+app.use(requestChecker);
 app.use(express.static('public'));
 app.get('/detectlanguage', executelangDetect);
 app.get('/searchapi', searchAPI);
